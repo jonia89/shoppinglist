ce1e8229259469dcece8f0faffdd6b16b85da6ba
jonia89 <jonianttila89@hotmail.com>
2023-03-27 15:40:46 +0300

    items & checked arrays are now in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Rootpage from "./pages/rootPage";
 import Listpage from "./pages/listPage";
@@ -6,16 +6,18 @@ import Addpage from "./pages/addPage";
 const items = ["Pizza", "Leipä, ruis", "Maito"].sort();
 const checked = ["Kaljaa", "Tonnikalaa", "Majoneesia"].sort();
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Rootpage />} />
-        <Route path="/list" element={<Listpage />} />
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Rootpage />} />
+          <Route path="/list" element={<Listpage />} />
 
-        <Route path="/add" element={<Addpage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+          <Route path="/add" element={<Addpage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 export { items, checked };
