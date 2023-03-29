2e7b827e8226f50eca25e4bf127e04a38c2cda43
jonia89 <jonianttila89@hotmail.com>
2023-03-29 16:27:21 +0300

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Rootpage from "./pages/rootPage";
 import Listpage from "./pages/listPage";
@@ -8,16 +8,16 @@ const items = ["Pizza", "Leipä, ruis", "Maito"].sort();
 const checked = ["Kaljaa", "Tonnikalaa", "Majoneesia"].sort();
 
 export default function App() {
-  
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Rootpage />} useState={[items, checked]}/>
-          <Route path="/list" element={<Listpage />} />
-          <Route path="/add" element={<Addpage />} onChange={useState()} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Rootpage />} useState={[items, checked]} />
+        <Route path="/list" element={<Listpage />} />
+       
+        <Route path="/add" element={<Addpage />} onChange={useState()} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export { items, checked };
