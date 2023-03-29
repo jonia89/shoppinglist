96b2210ce2d05d7628bdac765cc721eb7f76d5fd
jonia89 <jonianttila89@hotmail.com>
2023-03-29 16:32:02 +0300

    ...

--- frontend/src/components/itemsLeft.jsx
+++ frontend/src/components/itemsLeft.jsx
@@ -1,41 +1,48 @@
-import React, { useState, setState } from "react";
+import React, { Component } from "react";
+import { items, checked } from "../App";
 
-const Left = () => {
-  const [items, checked] = useState([])
-  const textStyle = {
+export default class Left extends Component {
+  textStyle = {
     fontSize: "25px",
     color: "red",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
   };
+  constructor(props) {
+    super(props);
 
-  const leftToGo = () => {
-    if (items.length === 0 && checked.length > 0)
+    this.state = {
+      items: [...items],
+      checked: [...checked]
+    };
+  }
+  leftToGo = () => {
+    if (this.state.items.length === 0 && this.state.checked.length > 0)
       return "Kaikki ostokset hoidettu, on aika suunnata kassalle";
-    else if (items.length === 1)
+    else if (this.state.items.length === 1)
       return (
         "Vielä " +
-        items.length +
+        this.state.items.length +
         " \ntuote jäljellä"
       );
-    else if (items.length > 1)
+    else if (this.state.items.length > 1)
       return (
-        "Vielä " + items.length + " \ntuotetta jäljellä"
+        "Vielä " + this.state.items.length + " \ntuotetta jäljellä"
       );
-      else if (items.length + checked.length === 0)
+      else if (this.state.items.length + this.state.checked.length === 0)
       return ("Siirry muokkaamaan listaa")
-    else if (items.length < 0)
+    else if (this.state.items.length < 0)
       return "Nyt on jotain pahasti pielessä";
   };
 
-  
+  render() {
     return (
       <div>
-        <span onChange={() => setState()} style={textStyle} className="badge badge-primary m-5">
-          {leftToGo()}
+        <span style={this.textStyle} className="badge badge-primary m-5">
+          {this.leftToGo()}
         </span>
       </div>
     );
   }
-  export default Left
+}
