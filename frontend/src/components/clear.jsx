dc18fb50d8910046b2d83207e252dab7bfeefb39
jonia89 <jonianttila89@hotmail.com>
2023-03-29 16:28:57 +0300

    ...

--- frontend/src/components/clear.jsx
+++ frontend/src/components/clear.jsx
@@ -1,33 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import { items, checked } from "../App";
 
-const Clear = () => {
-  const clearAll = () => {
+export default class Clear extends Component {
+  constructor(props) {
+    super(props);
+    this.clearAll.bind(this);
+    this.state = {
+      items: [...items],
+      checked: [...checked]
+    }
+  }
+
+  clearAll = () => {
     console.log("Tyhjentää listan");
     console.log(this.state.items, this.state.checked);
     const areYouSure = prompt("Oletko varma? (kirjoita kyllä vahvistaaksesi)");
     if (areYouSure === "kyllä") {
       while (this.state.items.length > 0) {
-        this.props.items.pop();
+        items.pop();
         while (this.state.checked.length > 0) {
-          this.props.checked.pop();
+          checked.pop();
         }
       }
       if (this.state.items.length === 0 && this.state.checked.length === 0) {
-        console.log(this.props.items, this.props.checked);
+        console.log(items, checked);
         return alert("Lista tyhjennetty");
       } else {
         return this.state.items && this.state.checked;
       }
     }
   };
-
-  return (
-    <div>
-      <button onDoubleClick={clearAll()} className="btn btn-danger btn-m m-2">
-        Tyhjennä tuplaklikkaamalla
-      </button>
-    </div>
-  );
-};
-
-export default Clear;
+  render() {
+    return (
+      <div>
+        <button
+          onDoubleClick={this.clearAll}
+          className="btn btn-danger btn-m m-2"
+        >
+          Tyhjennä tuplaklikkaamalla
+        </button>
+      </div>
+    );
+  }
+}
